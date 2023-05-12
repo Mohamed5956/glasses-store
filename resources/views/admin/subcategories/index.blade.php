f3cd99fa72a1edd9d064baa1c2e92d2fc36072aa
Mohammed Adel <mohammedadell301@gmail.com>
2023-05-12 14:52:07 +0300

    first commit

---
+++ resources/views/admin/subcategories/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Subcategories') }}</div>
+
+                    <div class="card-body">
+                        <div class="d-flex justify-content-end mb-3">
+                            <a href="{{ route('subcategories.create') }}" class="btn btn-primary">{{ __('Create Subcategory') }}</a>
+                        </div>
+
+                        @if (count($subcategories) > 0)
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">{{ __('Name') }}</th>
+                                    <th scope="col">{{ __('Slug') }}</th>
+                                    <th scope="col">{{ __('Actions') }}</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($subcategories as $subcategory)
+                                    <tr>
+                                        <td>{{ $subcategory->name }}</td>
+                                        <td>{{ $subcategory->slug }}</td>
+                                        <td>
+                                            <a href="{{ route('subcategories.edit', $subcategory) }}" class="btn btn-sm btn-primary">{{ __('Edit') }}</a>
+
+                                            <form class="d-inline" method="POST" action="{{ route('subcategories.destroy', $subcategory) }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('{{ __('Are you sure?') }}')">{{ __('Delete') }}</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        @else
+                            <p>No subcategories found.</p>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
